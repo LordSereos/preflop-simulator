977c2158f281c0fb1b727ecffed9e66e16476cc1
unknown <martin.martijan@gmail.com>
2022-11-12 10:24:42 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Card card1 = new Card("ace","spades");
+        Card card2 = new Card("KinG","hearts");
+        System.out.println(card1.toString());
+        System.out.println(card2.toString());
+    }
+}
