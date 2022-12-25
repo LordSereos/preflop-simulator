0b6797b548115ebde602492a7468786977d2f680
unknown <martin.martijan@gmail.com>
2022-12-25 16:31:42 +0200

    Fixed determination bugs

--- src/main/java/CombinationFinder.java
+++ src/main/java/CombinationFinder.java
@@ -18,40 +18,52 @@ public class CombinationFinder {
     private boolean isRoyalFlush = false;
     private boolean areQuads = false;
     private boolean areTrips = false;
-    private String tripsFace;
     private boolean arePairs = false;
-    private String firstPairsFace;
-    private String secondPairsFace;
-    private String highCard;
+
+
+    private String firstFactor = "";
+    private String secondFactor = "";
+    private int thirdFactor = 0;
+    private List<Card> kickerList = new ArrayList<>();
 
     CombinationFinder(Table table, Player player){
         setHandAndTable(table, player);
        // System.out.printf(handAndTable.toString()+"\n");
         setValues();
         sortLowestToHighest();
-       // System.out.printf(handAndTable.toString());
+        System.out.printf(handAndTable.toString());
         checkIfFlush();
 
         if (spadesFlush || heartsFlush || diamondsFlush || clubsFlush) {
+
             reduceFor5CardsFlush();
             checkIfStraight();
             if (isStraight){
+                //System.out.println("\nFirst factor in straight1: " + firstFactor);
                 checkIfRoyal();
                 if (isRoyalFlush){
                     System.out.println("\nIS ROYAL");
-                } else System.out.println("\nIS STRAIGHT FLUSH");
+                } else {
+                    isStraightFlush = true;
+                    System.out.println("\nHighest in straight flush: " + firstFactor);
+                    System.out.println("\nIS STRAIGHT FLUSH");
+                }
             }
-            else System.out.println("\nIS FLUSH");
+            else {
+                System.out.println("\nHighest in flush: " + firstFactor);
+                System.out.println("\nIS FLUSH");
+            }
         }
         else {
             checkIfStraight();
             if (isStraight){
+                System.out.println("\nHighest is straight: " + firstFactor);
                 System.out.println("\nIS STRAIGHT");
             }
             else {
                 checkIfQuads();
                 if (areQuads){
-                    System.out.println("\nARE QUADS");
+                    System.out.println("\nARE QUADS " + firstFactor);
                 }
                 else {
                     checkIfTrips();
@@ -60,27 +72,44 @@ public class CombinationFinder {
                         checkIfPairs();
                         if (arePairs){
                             System.out.println("\nFULL HOUSE");
-                            System.out.println("\nTrips: " + tripsFace);
-                            System.out.println("\nPair: " + firstPairsFace);
+                            System.out.println("\nTrips: " + firstFactor);
+                            System.out.println("\nPair: " + secondFactor);
                         }
-                        else System.out.println("\nARE TRIPS");
+                        else {
+                            findThirdFactor();
+                        //    System.out.println("\nThird factor in trips: " + thirdFactor);
+                            System.out.println("\nKicker: " + kickerList + "   (value is " + thirdFactor + ")");
+                            System.out.println("\nARE TRIPS " + firstFactor);
+                        }
                     }
                     else {
                         checkIfPairs();
                         if (arePairs){
                             arePairs=false;
-                            reduce2FoundPairs();
+                            reduce2FoundPairs(firstFactor);
+                            //System.out.println("\n"+handAndTable.toString());
                             checkIfSecondPairs();
                             if (arePairs){
+                                reduce2FoundPairs(secondFactor);
+                                findThirdFactor();
+                             //   System.out.println("\n"+handAndTable.toString());
                                 System.out.println("\nARE TWO PAIRS");
-                                System.out.println("\nBigger pair: " + firstPairsFace);
-                                System.out.println("\nSmaller pair: " + secondPairsFace);
+                                System.out.println("\nBigger pair: " + firstFactor);
+                                System.out.println("\nSmaller pair: " + secondFactor);
+                            //    System.out.println("\nThird factor: " + thirdFactor);
+                                System.out.println("\nKicker: " + kickerList + "   (value is " + thirdFactor + ")");
                             }
-                            else System.out.println("\nARE PAIRS");
+                            else {
+                                findThirdFactor();
+                                System.out.println("\nPair: " + firstFactor);
+                            //    System.out.println("\nThird factor in pairs: " + thirdFactor);
+                                System.out.println("\nKicker: " + kickerList + "   (value is " + thirdFactor + ")");
+                                System.out.println("\nARE PAIRS");
+                            }
                         }
                         else {
                             findHighCard();
-                            System.out.println("\nHIGH CARD " + highCard);
+                            System.out.println("\nHIGH CARD " + firstFactor);
                         }
                     }
                 }
@@ -141,7 +170,7 @@ public class CombinationFinder {
         int countHearts = 0;
         int countSpades = 0;
 
-        for (int i=0;i<7;i++){
+        for (int i=handAndTable.size()-1;i>=0;i--){
             if (handAndTable.get(i).getSuit().equals("diamonds")){
                 countDiamonds++;
             }
@@ -157,15 +186,39 @@ public class CombinationFinder {
         }
         if (countClubs >=5 ){
             clubsFlush=true;
+            for (int i=handAndTable.size()-1;i>=0;i--) {
+                if (handAndTable.get(i).getSuit().equals("clubs")) {
+                    firstFactor = handAndTable.get(i).getFaceName();
+                    break;
+                }
+            }
         }
         if (countDiamonds >=5 ){
             diamondsFlush=true;
+            for (int i=handAndTable.size()-1;i>=0;i--) {
+                if (handAndTable.get(i).getSuit().equals("diamonds")) {
+                    firstFactor = handAndTable.get(i).getFaceName();
+                    break;
+                }
+            }
         }
         if (countHearts >=5 ){
             heartsFlush=true;
+            for (int i=handAndTable.size()-1;i>=0;i--) {
+                if (handAndTable.get(i).getSuit().equals("hearts")) {
+                    firstFactor = handAndTable.get(i).getFaceName();
+                    break;
+                }
+            }
         }
         if (countSpades >=5 ){
             spadesFlush=true;
+            for (int i=handAndTable.size()-1;i>=0;i--) {
+                if (handAndTable.get(i).getSuit().equals("spades")) {
+                    firstFactor = handAndTable.get(i).getFaceName();
+                    break;
+                }
+            }
         }
     }
 
@@ -209,33 +262,56 @@ public class CombinationFinder {
         for (int i=hat.size()-1;i>=0;i--){
             handAndTable.add(hat.get(i));
         }
-        System.out.printf("\n"+handAndTable.toString());
+       // System.out.printf("\n"+handAndTable.toString());
     }
 
     public void checkIfStraight(){
-        if (isAceInHand()){
-            if (is2345InHand()){
+
+        if (isAceInHand()) {
+            if (is2345InHand()) {
                 isStraight = true;
+            }
+            else {
+                for (int i = handAndTable.size() - 1; i > 0; i--) {
+                    int q = 1;
+                    int sequenceCounter = 0;
+                    for (int j = i - 1; j >= 0; j--) {
+                        if (values.get(handAndTable.get(i).getFaceName()) - values.get(handAndTable.get(j).getFaceName()) == q) {
+                            sequenceCounter++;
+                            q++;
+                        }
+                        if (sequenceCounter == 4) {
+                            isStraight = true;
+                            if (firstFactor.equals("")) {
+                                firstFactor = handAndTable.get(i).getFaceName();
+                            }
+                            break;
+                        }
+                    }
+                    if (sequenceCounter == 4) break;
+                }
             }
         }
         else {
-            for (int i=handAndTable.size()-1;i>0;i--){
-                int q=1;
+            for (int i = handAndTable.size() - 1; i > 0; i--) {
+                int q = 1;
                 int sequenceCounter = 0;
-                for (int j=i-1;j>=0;j--){
-                    if (values.get(handAndTable.get(i).getFaceName()) - values.get(handAndTable.get(j).getFaceName()) == q){
+                for (int j = i - 1; j >= 0; j--) {
+                    if (values.get(handAndTable.get(i).getFaceName()) - values.get(handAndTable.get(j).getFaceName()) == q) {
                         sequenceCounter++;
                         q++;
                     }
-                    if (sequenceCounter==4){
+                    if (sequenceCounter == 4) {
                         isStraight = true;
+                        if (firstFactor.equals("")) {
+                            firstFactor = handAndTable.get(i).getFaceName();
+                        }
                         break;
                     }
                 }
-                if (sequenceCounter==4) break;
+                if (sequenceCounter == 4) break;
             }
         }
-
     }
 
     public void checkIfRoyal(){
@@ -255,6 +331,7 @@ public class CombinationFinder {
                 }
                 if (counter==3){
                     areQuads = true;
+                    firstFactor = handAndTable.get(i).getFaceName();
                     break;
                 }
             }
@@ -272,7 +349,7 @@ public class CombinationFinder {
                 }
                 if (counter==2){
                     areTrips = true;
-                    tripsFace = handAndTable.get(i).getFaceName();
+                    firstFactor = handAndTable.get(i).getFaceName();
                     break;
                 }
             }
@@ -281,7 +358,7 @@ public class CombinationFinder {
 
     public void reduce3FoundTrips(){
         for (int i=0;i<handAndTable.size();i++){
-            if (handAndTable.get(i).getFaceName().equals(tripsFace)){
+            if (handAndTable.get(i).getFaceName().equals(firstFactor)){
                 handAndTable.remove(handAndTable.get(i));
                 i--;
             }
@@ -289,49 +366,42 @@ public class CombinationFinder {
     }
 
     public void checkIfPairs(){
-
-        for (int i=0;i<handAndTable.size()-1;i++){
-            for (int j=i+1;j<handAndTable.size();j++){
+        for (int i=handAndTable.size()-1;i>0;i--){
+            for (int j=i-1;j>=0;j--){
                 if (handAndTable.get(j).getFaceName().equals(handAndTable.get(i).getFaceName())){
                     arePairs = true;
-                    firstPairsFace = handAndTable.get(i).getFaceName();
+                    if (!areTrips){
+                        firstFactor = handAndTable.get(i).getFaceName();
+                    } else {
+                        secondFactor = handAndTable.get(i).getFaceName();
+                    }
                     break;
                 }
             }
+        if (arePairs) break;
         }
     }
 
     public void checkIfSecondPairs(){
-
-        for (int i=0;i<handAndTable.size()-1;i++){
-            for (int j=i+1;j<handAndTable.size();j++){
+        for (int i=handAndTable.size()-1;i>=0;i--){
+            for (int j=i-1;j>=0;j--){
                 if (handAndTable.get(j).getFaceName().equals(handAndTable.get(i).getFaceName())){
                     arePairs = true;
-                    secondPairsFace = handAndTable.get(i).getFaceName();
+                    secondFactor = handAndTable.get(i).getFaceName();
                     break;
                 }
             }
+            if (arePairs) break;
         }
     }
 
-    public void reduce2FoundPairs(){
-        for (int i=0;i<handAndTable.size();i++){
-            if (handAndTable.get(i).getFaceName().equals(firstPairsFace)){
+    public void reduce2FoundPairs(String factor){
+        for (int i=handAndTable.size()-1;i>=0;i--){
+            if (handAndTable.get(i).getFaceName().equals(factor)){
                 handAndTable.remove(handAndTable.get(i));
-                i--;
+               // i--;
             }
         }
-        for (int i=0;i<handAndTable.size()-1;i++){
-            for (int j=i+1;j<handAndTable.size();j++){
-                if (handAndTable.get(j).getFaceName().equals(handAndTable.get(i).getFaceName())){
-                    arePairs = true;
-                    secondPairsFace = handAndTable.get(i).getFaceName();
-                    break;
-                }
-            }
-        }
-
-
     }
 
     public void findHighCard(){
@@ -341,7 +411,7 @@ public class CombinationFinder {
                 max = handAndTable.get(i);
             }
         }
-        highCard = max.getFaceName();
+        firstFactor = max.getFaceName();
     }
 
     public boolean isAceInHand(){
@@ -358,6 +428,7 @@ public class CombinationFinder {
         boolean three = false;
         boolean four = false;
         boolean five = false;
+        boolean six = false;
         for (Card c:handAndTable){
             if (c.getFaceName().equals("2")){
                 two = true;
@@ -378,7 +449,25 @@ public class CombinationFinder {
                 five = true;
             }
         }
-        if (two && three && four && five) return true;
+        for (Card c:handAndTable){
+            if (c.getFaceName().equals("6")){
+                six = true;
+            }
+        }
+        if (two && three && four && five && !six) {
+            firstFactor = "5";
+            return true;
+        }
         else return false;
+    }
+
+    public void findThirdFactor(){
+        //System.out.println("hat size:"+handAndTable.size());
+        int factorSize = handAndTable.size()-2;
+        //System.out.println("factor size:"+factorSize);
+        for (int i=handAndTable.size()-1;i>=handAndTable.size()-factorSize;i--){
+            thirdFactor += values.get(handAndTable.get(i).getFaceName());
+            kickerList.add(handAndTable.get(i));
+        }
     }
 }
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -4,21 +4,23 @@ public class main {
         for (int i=0;i<10;i++){
             deck.shuffleDeck();
         }
-        Card my1 = new Card("2", "diamonds");
-        Card my2 = new Card("5", "hearts");
+
+        Card my1 = new Card("5", "spades");
+        Card my2 = new Card("7", "clubs");
+
+        Card c1 = new Card("9", "hearts");
+        Card c2 = new Card("jack", "hearts");
+        Card c3 = new Card("king", "diamonds");
+        Card c4 = new Card("2", "hearts");
+        Card c5 = new Card("queen", "hearts");
+
         Player p1 = new Player(deck, my1, my2);
-
+        Player p2 = new Player(deck);
         System.out.println("My hand: "+"    "+p1.getHand().toString());
-
-        Card c1 = new Card("king", "spades");
-        Card c2 = new Card("9", "clubs");
-        Card c3 = new Card("4", "diamonds");
-        Card c4 = new Card("ace", "spades");
-        Card c5 = new Card("8", "hearts");
-
         Table table = new Table(deck,c1,c2,c3,c4,c5);
+        Table table1 = new Table(deck);
         System.out.println("Table cards: " + table.getCards().toString());
+
         CombinationFinder cf = new CombinationFinder(table, p1);
-        System.out.println(cf.getHandAndTable().toString());
     }
 }
